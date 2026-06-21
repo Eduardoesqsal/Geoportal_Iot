4b70a057809e40a85b7576b32dfcd1140692ecf5
Tu Nombre <eduardoesquivelsa@gmail.com>
2026-06-21 11:26:20 -0600

    fix: convert jest config to CommonJS for CI compatibility

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,5 @@
-import type { Config } from 'jest'
-
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(t|j)sx?$': ['@swc/jest', {
@@ -19,4 +18,4 @@ const config: Config = {
   testMatch: ['<rootDir>/src/__tests__/**/*.test.(ts|tsx)'],
 }
 
-export default config
+module.exports = config
